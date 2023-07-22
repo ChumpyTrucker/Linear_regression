1ca80c73854a5676ff319f05046d9827a8c55a59
ChumpyTrucker <87389714+ChumpyTrucker@users.noreply.github.com>
2023-07-22 23:28:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Generate random data
+np.random.seed(42)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Predict on new data points
+new_X = np.array([[0], [2]])
+predictions = model.predict(new_X)
+
+# Visualize the data and the fitted line
+plt.scatter(X, y, color='blue', label='Data points')
+plt.plot(new_X, predictions, color='red', label='Fitted line')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.title('Linear Regression Example')
+plt.legend()
+plt.show()
